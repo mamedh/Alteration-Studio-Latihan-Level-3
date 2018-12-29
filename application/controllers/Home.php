7d22447edc120d5b3af3573da774ca4780b3da7e
Muhammad <mamedh.hadem@gmail.com>
2018-12-29 12:46:22 +0700

    Pertemuan 5

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->model('produk_model');
+		$this->load->model('kategori_model');		
+
+		$produk = $this->produk_model->get();
+		$kategori = $this->kategori_model->get();
+		$data =[
+			'produk' => $produk,
+			'kategori' => $kategori
+		];
+
+		$this->load->view('home/index', $data);
+	}
+}
