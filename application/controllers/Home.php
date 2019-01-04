1118e05f99a00af6c99f1920fb29694bdf342dfa
Muhammad <mamedh.hadem@gmail.com>
2019-01-04 09:17:45 +0700

    PR Pertemuan 6

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		
+		if (empty($this->session->userdata('username'))) {
+			redirect('home_login');
+		}
+	}
+
 	public function index()
 	{
 		$this->load->model('produk_model');
